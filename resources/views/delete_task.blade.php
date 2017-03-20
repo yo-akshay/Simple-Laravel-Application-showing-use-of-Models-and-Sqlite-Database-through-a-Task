eb2523e62cd59f7458153186adfddbea053f794b
Akshay <akshay1234530@yahoo.com>
2017-03-20 12:02:12 -0700

    Update delete_task.blade.php

--- resources/views/delete_task.blade.php
+++ resources/views/delete_task.blade.php
@@ -4,7 +4,7 @@
 <h1>Delete Task</h1>
 <form action="/del">
   Enter the Task Title to be deleted:<br>
-  <input type="text" name="Title">
+  <input type="text" name="id">
   <br>
   <input type="submit" value="delete">
 </form> 
